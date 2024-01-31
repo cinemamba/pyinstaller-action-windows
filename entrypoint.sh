79ca7d971e29de5d5becd3d6f0a5878c13e83d66
András Bedő <61637721+cinemamba@users.noreply.github.com>
2024-01-31 01:31:09 +0100

    python -m PyInstaller

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,7 +46,7 @@ if [ -f $5 ]; then
 fi # [ -f $5 ]
 
 # if [[ "$@" == "" ]]; then
-python pyinstaller --clean -y --dist ./dist/windows --workpath /tmp $SPEC_FILE
+python -m PyInstaller --clean -y --dist ./dist/windows --workpath /tmp $SPEC_FILE
 pyinstaller --clean -y --dist ./dist/windows --workpath /tmp $SPEC_FILE
 chown -R --reference=. ./dist/windows
 # else
